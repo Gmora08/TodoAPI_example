9689b476da538e1f0063dd3753ab20b093496639
gmora08 <mnr.hh08@gmail.com>
2015-07-24 18:52:18 -0500

    add serializer format JSON

--- todolist/api.py
+++ todolist/api.py
@@ -1,12 +1,15 @@
 from tastypie.resources import ModelResource
+from tastypie.serializers import Serializer
 from .models import TodoList, TodoItem
 
 class TodoListResource(ModelResource):
   class Meta:
     queryset = TodoList.objects.all()
     resource_name = 'lists'
+    serializer = Serializer(formats=['json',])
 
 class TodoItemResource(ModelResource):
   class Meta:
     queryset = TodoItem.objects.all()
     resource_name = 'items'
+    serializer = Serializer(formats=['json',])
